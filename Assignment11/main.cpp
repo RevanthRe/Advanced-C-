a060443f601c116b4723e46ef2184cd474341e85
RevanthRe <149352194+RevanthRe@users.noreply.github.com>
2023-11-28 23:35:13 +0530

    Add files via upload

---
+++ Assignment11/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include "headers/Reader.h"
+#include "headers/Triangle.h"
+#include "headers/Triangulation.h"
+#include "headers/Writer.h"
+
+int main()
+{
+    std::vector<Triangle> triangles;
+    Reader reader("E:\\Ramesh__Workspace\\Sketcher Exercise\\EX11\\Updated Sketchers\\cubeModel.stl");
+    reader.getTriangles(triangles);
+
+    Triangulation triangulation(triangles);
+    triangulation.printArea();
+
+    Writer writer;
+
+    std::vector<Triangle> outTriangles=triangulation.getTriangles();
+    writer.write("E:\\Ramesh__Workspace\\Sketcher Exercise\\EX11\\Updated Sketchers\\output.txt", outTriangles);
+
+    std::cout<<"Press enter to exit\n";
+}
